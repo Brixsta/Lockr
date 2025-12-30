4eea7bf4c99844b0d63a28207a8306063ea01c80
Brixsta <Brixsta@gmail.com>
2025-12-29 21:20:38 -0600

    refactor GUI.py to make it easier to read

--- GUI.py
+++ GUI.py
@@ -4,6 +4,16 @@ import ProcessTable
 import utils
 import customtkinter as ctk
 
+# ------------------------- Constants ------------------------
+FONT_TITLE = ("Helvetica", 22, "bold")
+FONT_BUTTON = ("Helvetica", 16, "bold")
+FONT_STATUS = ("Helvetica", 12, "bold")
+BG_COLOR = "#EAEAEA"
+BTN_BG = "#F0F0F0"
+BTN_HOVER = "#DADADA"
+BORDER_COLOR = "#C0C0C0"
+TEXT_COLOR = "#1F2937"
+
 
 class GUI:
     def __init__(self, root):
@@ -24,15 +34,15 @@ class GUI:
 
     def create_frames(self):
         # Create main_frame
-        self.main_frame = tk.Frame(self.root, bg="#EAEAEA")
+        self.main_frame = tk.Frame(self.root, bg=BG_COLOR)
         self.main_frame.pack(fill="both", expand=True)
 
         # Create left_main_frame
-        self.left_main_frame = tk.Frame(self.main_frame, bg="#EAEAEA")
+        self.left_main_frame = tk.Frame(self.main_frame, bg=BG_COLOR)
         self.left_main_frame.place(relx=0, rely=0, relwidth=.5, relheight=1)
 
         # Create search_bar_frame
-        self.search_bar_frame = tk.Frame(self.left_main_frame, bg="#EAEAEA")
+        self.search_bar_frame = tk.Frame(self.left_main_frame, bg=BG_COLOR)
         self.search_bar_frame.pack(fill="x", padx=100, pady=(20, 0))
 
         # Create process_table_frame
@@ -40,31 +50,31 @@ class GUI:
         self.process_table_frame.pack(fill="both", expand=True, padx=20, pady=20)
 
         # Create refresh_table_frame
-        self.refresh_table_frame = tk.Frame(self.left_main_frame, bg="#EAEAEA")
+        self.refresh_table_frame = tk.Frame(self.left_main_frame, bg=BG_COLOR)
         self.refresh_table_frame.pack(fill="x", padx=0, pady=(0, 20))
 
         # Create right_main_frame
-        self.right_main_frame = tk.Frame(self.main_frame, bg="#EAEAEA")
+        self.right_main_frame = tk.Frame(self.main_frame, bg=BG_COLOR)
         self.right_main_frame.place(relx=.5, rely=0, relwidth=.5, relheight=1)
 
         # Create lock_actions_frame
-        self.lock_actions_frame = tk.Frame(self.right_main_frame, bg="#EAEAEA")
+        self.lock_actions_frame = tk.Frame(self.right_main_frame, bg=BG_COLOR)
         self.lock_actions_frame.pack(fill="both", expand=True, padx=(0, 20), pady=20)
 
         # Create selected_process_frame
-        self.selected_process_frame = tk.Frame(self.lock_actions_frame, bg="#EAEAEA")
+        self.selected_process_frame = tk.Frame(self.lock_actions_frame, bg=BG_COLOR)
         self.selected_process_frame.pack(fill="x", ipadx=10, ipady=10)
 
         # Create selected_process_status_frame
-        self.selected_process_status_frame = tk.Frame(self.lock_actions_frame, bg="#EAEAEA")
+        self.selected_process_status_frame = tk.Frame(self.lock_actions_frame, bg=BG_COLOR)
         self.selected_process_status_frame.pack(fill="x")
 
         # Create lock_buttons_frame
-        self.lock_buttons_frame = tk.Frame(self.lock_actions_frame, bg="#EAEAEA")
+        self.lock_buttons_frame = tk.Frame(self.lock_actions_frame, bg=BG_COLOR)
         self.lock_buttons_frame.pack(fill="x")
 
         # Create activate_lock_frame
-        self.activate_lock_frame = tk.Frame(self.lock_actions_frame, bg="#EAEAEA")
+        self.activate_lock_frame = tk.Frame(self.lock_actions_frame, bg=BG_COLOR)
         self.activate_lock_frame.pack(fill="x")
 
     def configure_frames(self):
@@ -89,7 +99,7 @@ class GUI:
         self.selected_process_status_label = ctk.CTkLabel(
             self.selected_process_status_frame,
             text="RUNNING",
-            font=("Helvetica", 12, "bold"),
+            font=FONT_STATUS,
             text_color="Green",
         )
         self.selected_process_status_label.pack()
@@ -98,15 +108,15 @@ class GUI:
         self.selected_process_name_label = ctk.CTkLabel(
             self.selected_process_frame,
             text=f"Process Name:",
-            font=("Helvetica", 22, "bold")
+            font=FONT_TITLE
         )
         self.selected_process_name_label.grid(column=1, row=1)
 
         # Create search_bar_entry
         self.search_var = ctk.StringVar()
         self.search_bar_entry = ctk.CTkEntry(self.search_bar_frame, placeholder_text="Enter process name here",
-                                             font=("Helvetica", 14), border_width=1, border_color="#C0C0C0",
-                                             text_color="#1F2937", textvariable=self.search_var)
+                                             font=("Helvetica", 14), border_width=1, border_color=BORDER_COLOR,
+                                             text_color=TEXT_COLOR, textvariable=self.search_var)
         self.search_bar_entry.pack(fill="x", ipadx=10, ipady=5)
         self.search_var.trace_add("write", self.handle_search_bar_input)
 
@@ -128,7 +138,7 @@ class GUI:
                                                   fg_color="forestgreen",
                                                   hover_color="#006400",
                                                   cursor="hand2",
-                                                  font=("Helvetica", 16, "bold"),
+                                                  font=FONT_BUTTON,
                                                   height=50,
                                                   command=lambda: utils.handle_refresh_button_click(self.process_table,
                                                                                                     self.handle_search_bar_input)
@@ -139,87 +149,34 @@ class GUI:
         self.lock_buttons_list = []
 
         # Create one_hour_lock_button
-        self.one_hour_lock_button = ctk.CTkButton(
-            self.lock_buttons_frame,
-            text="1 Hour",
-            text_color="#1F2937",
-            fg_color="#DADADA",
-            hover_color="#DADADA",
-            border_width=1,
-            border_color="#C0C0C0",
-            cursor="hand2",
-            font=("Helvetica", 16, "bold"),
-            height=50,
-            command=lambda: utils.toggle_lock_buttons(self.one_hour_lock_button, self.lock_buttons_list)
-        )
+        self.one_hour_lock_button = self.create_lock_button("1 Hour", command=lambda: utils.toggle_lock_buttons(
+            self.one_hour_lock_button, self.lock_buttons_list))
         self.one_hour_lock_button.pack(fill="x", pady=10)
         self.lock_buttons_list.append(self.one_hour_lock_button)
 
         # Create two_hour_lock_button
-        self.two_hour_lock_button = ctk.CTkButton(
-            self.lock_buttons_frame,
-            text="2 Hours",
-            text_color="#1F2937",
-            fg_color="#F0F0F0",
-            hover_color="#DADADA",
-            border_width=1,
-            border_color="#C0C0C0",
-            cursor="hand2",
-            font=("Helvetica", 16, "bold"),
-            height=50,
-            command=lambda: utils.toggle_lock_buttons(self.two_hour_lock_button, self.lock_buttons_list)
-        )
+        self.two_hour_lock_button = self.create_lock_button("2 Hours", command=lambda: utils.toggle_lock_buttons(
+            self.two_hour_lock_button, self.lock_buttons_list))
         self.two_hour_lock_button.pack(fill="x", pady=10)
         self.lock_buttons_list.append(self.two_hour_lock_button)
 
         # Create three_hour_lock_button
-        self.four_hour_lock_button = ctk.CTkButton(
-            self.lock_buttons_frame,
-            text="4 Hours",
-            text_color="#1F2937",
-            fg_color="#F0F0F0",
-            hover_color="#DADADA",
-            border_width=1,
-            border_color="#C0C0C0",
-            cursor="hand2",
-            font=("Helvetica", 16, "bold"),
-            height=50,
-            command=lambda: utils.toggle_lock_buttons(self.four_hour_lock_button, self.lock_buttons_list)
-        )
-        self.four_hour_lock_button.pack(fill="x", pady=10)
-        self.lock_buttons_list.append(self.four_hour_lock_button)
+        self.three_hour_lock_button = self.create_lock_button("3 Hours", command=lambda: utils.toggle_lock_buttons(
+            self.three_hour_lock_button, self.lock_buttons_list))
+        self.three_hour_lock_button.pack(fill="x", pady=10)
+        self.lock_buttons_list.append(self.three_hour_lock_button)
 
         # Create eight_hour_lock_button
-        self.eight_hour_lock_button = ctk.CTkButton(
-            self.lock_buttons_frame,
-            text="8 Hours",
-            text_color="#1F2937",
-            fg_color="#F0F0F0",
-            hover_color="#DADADA",
-            border_width=1,
-            border_color="#C0C0C0",
-            cursor="hand2",
-            font=("Helvetica", 16, "bold"),
-            height=50,
-            command=lambda: utils.toggle_lock_buttons(self.eight_hour_lock_button, self.lock_buttons_list)
-        )
+        self.eight_hour_lock_button = self.create_lock_button("8 Hours", command=lambda: utils.toggle_lock_buttons(
+            self.eight_hour_lock_button, self.lock_buttons_list))
         self.eight_hour_lock_button.pack(fill="x", pady=10)
         self.lock_buttons_list.append(self.eight_hour_lock_button)
 
-        # Create custom_lock_button
-        self.twentyfour_hour_lock_button = ctk.CTkButton(
-            self.lock_buttons_frame,
-            text="24 Hours",
-            text_color="#1F2937",
-            fg_color="#F0F0F0",
-            hover_color="#DADADA",
-            border_width=1,
-            border_color="#C0C0C0",
-            cursor="hand2",
-            font=("Helvetica", 16, "bold"),
-            height=50,
-            command=lambda: utils.toggle_lock_buttons(self.twentyfour_hour_lock_button, self.lock_buttons_list)
-        )
+        # Create twentyfour_hour_lock_button
+        self.twentyfour_hour_lock_button = self.create_lock_button("24 Hours",
+                                                                   command=lambda: utils.toggle_lock_buttons(
+                                                                       self.twentyfour_hour_lock_button,
+                                                                       self.lock_buttons_list))
         self.twentyfour_hour_lock_button.pack(fill="x", pady=10)
         self.lock_buttons_list.append(self.twentyfour_hour_lock_button)
 
@@ -227,7 +184,7 @@ class GUI:
         self.confirm_lock_button = ctk.CTkButton(
             self.activate_lock_frame,
             text="Lock",
-            font=("Helvetica", 16, "bold"),
+            font=FONT_BUTTON,
             height=50,
             corner_radius=8,
             cursor="hand2",
@@ -258,4 +215,18 @@ class GUI:
         # Paint the alternating row and locked effect
         table.paint_alternating_rows()
 
-
+    def create_lock_button(self, text, command):
+        btn = ctk.CTkButton(
+            self.lock_buttons_frame,
+            text=text,
+            text_color=TEXT_COLOR,
+            fg_color=BTN_BG,
+            hover_color=BTN_HOVER,
+            border_width=1,
+            border_color=BORDER_COLOR,
+            cursor="hand2",
+            font=FONT_BUTTON,
+            height=50,
+            command=command
+        )
+        return btn
